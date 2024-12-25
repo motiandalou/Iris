296737feee824b57160352ae0169aaa6214a023b
motiandalou <66056694+motiandalou@users.noreply.github.com>
2024-12-25 16:40:11 +0800

    Add files via upload
    1. 点击 View More 打开对应的弹窗,之前的效果是一直是第一个弹窗

--- carousel.js
+++ carousel.js
@@ -1,18 +1,24 @@
 // 轮播
 let slideIndex = 0;
 
-function showSlides(slides) {
+function showSlides(slides, now = 0) {
   let i;
+
+  // 默认全部隐藏
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
+
+  if (now) {
+    slides[now - 1].style.display = "flex";
+  } else {
+    slides[0].style.display = "flex";
   }
-  slides[slideIndex - 1].style.display = "flex";
+
+  // onclick = 'showSlides(document.querySelectorAll("#dialog-EngTUTOR .slide"),4)';
 }
 
+// 切换--上一页 / 下一页
 function moveSlide(n, id) {
   slideIndex += n;
   let slides = document.querySelectorAll(`#${id} .slide`);
